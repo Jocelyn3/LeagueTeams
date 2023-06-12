98f6346d7fa9e31697e76ad3a80f87fa98220268
jaguemon <jocelyn.aguemon@ingenico.com>
2023-06-12 10:03:22 +0200

    Last check

--- app/src/main/java/fr/fdj/leagueteams/screen/Screen.kt
+++ app/src/main/java/fr/fdj/leagueteams/screen/Screen.kt
@@ -1,7 +1,6 @@
 package fr.fdj.leagueteams.screen
 
 sealed class Screen(val route: String) {
-    object SearchScreen: Screen("search_screen")
     object TeamListScreen: Screen("team_list_screen")
 
 }
